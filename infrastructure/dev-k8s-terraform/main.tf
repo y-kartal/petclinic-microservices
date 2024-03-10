39a1e999ac3553c8bee948d581a3ff65d5c14383
y-kartal <yasinkartal2009@gmail.com>
2024-03-10 08:08:16 +0000

    maintf update

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -155,7 +155,7 @@ resource "aws_iam_instance_profile" "petclinic-master-server-profile1" {
 resource "aws_instance" "kube-master" {
     ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t3a.medium"
-    iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
+    iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile1.name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
     subnet_id = "subnet-02ee252b1b677f1d7"  # select own subnet_id of us-east-1a
